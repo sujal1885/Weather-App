20abe09ce491b2e58cb6dcf9d780faff4e1c2fb0
sujal <sujalagrawalgondia@gmail.com>
2023-02-18 03:43:24 +0530

    updated package.json

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppBar, Toolbar ,Typography , makeStyles} from "@material-ui/core";
+import { AppBar, Toolbar ,Typography , makeStyles} from "@mui/material";
 
 
 const header = () => {
